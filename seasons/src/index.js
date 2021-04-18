35638dea196519b5f87648a984ada148d14805c2
Matt Gilbert <mattgilbert042@gmail.com>
2021-04-18 16:29:27 -0500

    section 6 - replacing constructor with simple state assignment

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // THIS IS THE ONLY TIME we perform a direct assignment to this.state
-    this.state = { lat: null, errorMessage: '' };
-  }
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     console.log('MOUNTED - componentDidMount');
