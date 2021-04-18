a48e6ace931c5baeacf75a2e65e0df6058a96f09
Matt Gilbert <mattgilbert042@gmail.com>
2021-04-18 16:25:19 -0500

    section 6 - moved geolocation code to componentDidMount function since the componentDidMount function is best for data loading

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,23 +7,18 @@ class App extends React.Component {
 
     // THIS IS THE ONLY TIME we perform a direct assignment to this.state
     this.state = { lat: null, errorMessage: '' };
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message })
-      }
-    );
   }
 
   componentDidMount() {
-    console.log('My component was rendered to the screen!');
+    console.log('MOUNTED - componentDidMount');
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
   }
 
   componentDidUpdate() {
-    console.log('My component was just updated - it rerendered!');
+    console.log('UPDATED - componentDidUpdate');
   }
 
   // React says we have to definte render!
