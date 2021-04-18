0f308b373904a414b44bfdc2d1d658fb84c5e83f
Matt Gilbert <mattgilbert042@gmail.com>
2021-04-18 16:16:40 -0500

    section 6 - learning about componentDidMount and componentDidUpdate

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -18,6 +18,14 @@ class App extends React.Component {
     );
   }
 
+  componentDidMount() {
+    console.log('My component was rendered to the screen!');
+  }
+
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered!');
+  }
+
   // React says we have to definte render!
   render() {
     if (this.state.errorMessage && !this.state.lat) {
