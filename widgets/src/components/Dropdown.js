65617041255e6982f6898ae7e5d9600af1a05d3b
Matt Gilbert <mattgilbert042@gmail.com>
2021-04-26 10:34:30 -0500

    section 12 - incorporated a state selection for the dropdown component

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ selected, onSelectedChange, options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        key={option.color}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <h1>
-        Dropdown JS
-      </h1>
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className="menu visible transition">
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Dropdown;
