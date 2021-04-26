230b772212dc16141f23f4360190c3c60d2f88e0
Matt Gilbert <mattgilbert042@gmail.com>
2021-04-26 13:56:57 -0500

    section 12 - created convert component and incorporated the google api call and key

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,19 +1,12 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
-  {
-    label: 'Afrikaans',
-    value: 'af'
-  },
-  {
-    label: 'Arabic',
-    value: 'ar'
-  },
-  {
-    label: 'Hindi',
-    value: 'hi'
-  }
+  { label: 'Afrikaans', value: 'af' },
+  { label: 'Arabic', value: 'ar' },
+  { label: 'Hindi', value: 'hi' },
+  { label: 'Dutch', value: 'nl' }
 ]
 
 const Translate = () => {
@@ -38,6 +31,10 @@ const Translate = () => {
         onSelectedChange={setLanguage}
         options={options}
       />
+
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
     </div>
   )
 };
