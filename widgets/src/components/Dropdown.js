a52d82350dcb5884035acf15e6850a0a920c8d28
Matt Gilbert <mattgilbert042@gmail.com>
2021-04-26 10:23:26 -0500

    section 12 - creating scaffolding for dropdown widget

---
+++ widgets/src/components/Dropdown.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dropdown = ({ options }) => {
+  return (
+    <div>
+      <h1>
+        Dropdown JS
+      </h1>
+    </div>
+  )
+};
+
+export default Dropdown;
