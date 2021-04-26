9e0d866737ff3c8a770cfb96dcab088d5b481985
Matt Gilbert <mattgilbert042@gmail.com>
2021-04-26 10:44:47 -0500

    section 12 - implemented state for manipulating the open and close visibility of the dropdown component

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({ selected, onSelectedChange, options }) => {
+  const [open, setOpen] = useState(false);
+
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -8,7 +10,7 @@ const Dropdown = ({ selected, onSelectedChange, options }) => {
 
     return (
       <div
-        key={option.color}
+        key={option.value}
         className="item"
         onClick={() => onSelectedChange(option)}
       >
@@ -21,10 +23,13 @@ const Dropdown = ({ selected, onSelectedChange, options }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
             {renderedOptions}
           </div>
         </div>
